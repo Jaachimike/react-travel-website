3a40e83d876150579357319c55b769acf3e0902a
Jaachi Okafor <okaforjaachi@gmail.com>
2023-06-29 21:38:06 +0100

    Added Navbar and Hero Section

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Hero.module.css";
+import { AiOutlineSearch } from "react-icons/ai";
+
+function hero() {
+  return (
+    <div className={styles.hero}>
+      <form action=""></form>
+    </div>
+  );
+}
+
+export default hero;
